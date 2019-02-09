5f93b1337911114e0914f12b3ac9a705832b7551
Justin Wood <justin.k.wood@protonmail.com>
2019-02-09 16:14:43 -0500

    Super simple rebar3 release structure
    Will still need to add the remaining options at a later time

--- src/beam2nix.erl
+++ src/beam2nix.erl
@@ -1,13 +1,63 @@
 -module(beam2nix).
 
 %% API exports
--export([]).
+-export([new/3]).
 
+-import(prettypr, [above/2, text/1]).
+
+-record(app, {name, vsn, src}).
 %%====================================================================
 %% API functions
 %%====================================================================
-
+-spec new(atom(), string(), string()) -> prettypr:document().
+new(AppName, Vsn, Src) ->
+    App = #app{name = AppName, vsn = Vsn, src = Src},
+    above(
+      header(),
+      derivation(App)
+     ).
 
 %%====================================================================
 %% Internal functions
 %%====================================================================
+-spec header() -> prettypr:document().
+header() ->
+    above(
+      text("{ rebar3Relx }:"),
+      text("")
+     ).
+
+-spec derivation(#app{}) -> prettypr:document().
+derivation(App) ->
+    above(
+      text("rebar3Relx {"),
+      above(
+        nest(body(App)),
+        text("}")
+           )
+     ).
+
+-spec body(#app{}) -> prettypr:document().
+body(App) ->
+    Chunks = [
+              kv("name", quote(atom_to_list(App#app.name))),
+              kv("version", quote(App#app.vsn)),
+              kv("src", App#app.src)
+             ],
+    lists:foldr(fun(Doc, Acc) ->
+                        above(Doc, Acc)
+                end, prettypr:empty(), Chunks).
+
+-spec quote(string()) -> prettypr:document().
+quote(Value) ->
+    prettypr:beside(text("\""), prettypr:beside(text(Value), text("\""))).
+
+-spec kv(string(), string() | prettypr:document()) -> prettypr:document().
+kv(Key, Value) when is_list(Value) ->
+    prettypr:sep([text(Key), text("="), text(Value)]);
+kv(Key, Value) ->
+    prettypr:sep([text(Key), text("="), Value]).
+
+-spec nest(prettypr:document()) -> prettypr:document().
+nest(Document) ->
+    prettypr:nest(2, Document).
